28395c9a19be5a88d2d525dc161927a1ac7a5419
Deep Ghag <deepghag48@gmail.com>
2025-03-10 09:36:30 +0530

    Add files via upload

---
+++ object/jiosaavn/jiosaavn.js
@@ -0,0 +1,233 @@
+let jiosaavn = {
+    "success": true,
+    "data": {
+      "topQuery": {
+        "results": [
+          {
+            "id": "UleloXwf",
+            "title": "Channa Ve",
+            "image": [
+              {
+                "quality": "50x50",
+                "url": "https://c.saavncdn.com/006/Bhoot-Part-One-The-Haunted-Ship-Hindi-2020-20200212161346-50x50.jpg"
+              },
+              {
+                "quality": "150x150",
+                "url": "https://c.saavncdn.com/006/Bhoot-Part-One-The-Haunted-Ship-Hindi-2020-20200212161346-150x150.jpg"
+              },
+              {
+                "quality": "500x500",
+                "url": "https://c.saavncdn.com/006/Bhoot-Part-One-The-Haunted-Ship-Hindi-2020-20200212161346-500x500.jpg"
+              }
+            ],
+            "album": "Bhoot - Part One: The Haunted Ship",
+            "url": "https://www.jiosaavn.com/song/channa-ve/JQQOXRtoQFU",
+            "type": "song",
+            "language": "hindi",
+            "description": "Song by Akhil Sachdeva and Mansheel Gujral",
+            "primaryArtists": "Akhil Sachdeva, Mansheel Gujral",
+            "singers": "Akhil Sachdeva, Mansheel Gujral"
+          }
+        ],
+        "position": 0
+      },
+      "songs": {
+        "results": [
+          {
+            "id": "UleloXwf",
+            "title": "Channa Ve",
+            "image": [
+              {
+                "quality": "50x50",
+                "url": "https://c.saavncdn.com/006/Bhoot-Part-One-The-Haunted-Ship-Hindi-2020-20200212161346-50x50.jpg"
+              },
+              {
+                "quality": "150x150",
+                "url": "https://c.saavncdn.com/006/Bhoot-Part-One-The-Haunted-Ship-Hindi-2020-20200212161346-150x150.jpg"
+              },
+              {
+                "quality": "500x500",
+                "url": "https://c.saavncdn.com/006/Bhoot-Part-One-The-Haunted-Ship-Hindi-2020-20200212161346-500x500.jpg"
+              }
+            ],
+            "album": "Bhoot - Part One: The Haunted Ship",
+            "url": "https://www.jiosaavn.com/song/channa-ve/JQQOXRtoQFU",
+            "type": "song",
+            "description": "Bhoot - Part One: The Haunted Ship · Akhil Sachdeva, Mansheel Gujral",
+            "primaryArtists": "Akhil Sachdeva, Mansheel Gujral",
+            "singers": "Akhil Sachdeva, Mansheel Gujral",
+            "language": "hindi"
+          },
+          {
+            "id": "K8enIY92",
+            "title": "Channa Ve",
+            "image": [
+              {
+                "quality": "50x50",
+                "url": "https://c.saavncdn.com/310/Sufna-Punjabi-2020-20200218113958-50x50.jpg"
+              },
+              {
+                "quality": "150x150",
+                "url": "https://c.saavncdn.com/310/Sufna-Punjabi-2020-20200218113958-150x150.jpg"
+              },
+              {
+                "quality": "500x500",
+                "url": "https://c.saavncdn.com/310/Sufna-Punjabi-2020-20200218113958-500x500.jpg"
+              }
+            ],
+            "album": "Sufna",
+            "url": "https://www.jiosaavn.com/song/channa-ve/O1AOXz1pDgE",
+            "type": "song",
+            "description": "Sufna · B Praak",
+            "primaryArtists": "B Praak",
+            "singers": "B Praak",
+            "language": "punjabi"
+          },
+          {
+            "id": "UM76ojoe",
+            "title": "Channa Ve",
+            "image": [
+              {
+                "quality": "50x50",
+                "url": "https://c.saavncdn.com/137/Channa-Ve-Hindi-2024-20240904130626-50x50.jpg"
+              },
+              {
+                "quality": "150x150",
+                "url": "https://c.saavncdn.com/137/Channa-Ve-Hindi-2024-20240904130626-150x150.jpg"
+              },
+              {
+                "quality": "500x500",
+                "url": "https://c.saavncdn.com/137/Channa-Ve-Hindi-2024-20240904130626-500x500.jpg"
+              }
+            ],
+            "album": "Channa Ve",
+            "url": "https://www.jiosaavn.com/song/channa-ve/JSVcBxtaWFY",
+            "type": "song",
+            "description": "Channa Ve · Rahul Sathu, Talha Anjum",
+            "primaryArtists": "Rahul Sathu, Talha Anjum",
+            "singers": "Rahul Sathu, Talha Anjum",
+            "language": "hindi"
+          }
+        ],
+        "position": 2
+      },
+      "albums": {
+        "results": [
+          {
+            "id": "57669492",
+            "title": "Channa Ve",
+            "image": [
+              {
+                "quality": "50x50",
+                "url": "https://c.saavncdn.com/137/Channa-Ve-Hindi-2024-20240904130626-50x50.jpg"
+              },
+              {
+                "quality": "150x150",
+                "url": "https://c.saavncdn.com/137/Channa-Ve-Hindi-2024-20240904130626-150x150.jpg"
+              },
+              {
+                "quality": "500x500",
+                "url": "https://c.saavncdn.com/137/Channa-Ve-Hindi-2024-20240904130626-500x500.jpg"
+              }
+            ],
+            "artist": "Rahul Sathu, Talha Anjum",
+            "url": "https://www.jiosaavn.com/album/channa-ve/E0dr58ZDOF4_",
+            "type": "album",
+            "description": "2024 · Hindi Album · Rahul Sathu, Talha Anjum",
+            "year": "2024",
+            "songIds": "UM76ojoe",
+            "language": "hindi"
+          },
+          {
+            "id": "19034342",
+            "title": "Bhoot - Part One: The Haunted Ship",
+            "image": [
+              {
+                "quality": "50x50",
+                "url": "https://c.saavncdn.com/006/Bhoot-Part-One-The-Haunted-Ship-Hindi-2020-20200212161346-50x50.jpg"
+              },
+              {
+                "quality": "150x150",
+                "url": "https://c.saavncdn.com/006/Bhoot-Part-One-The-Haunted-Ship-Hindi-2020-20200212161346-150x150.jpg"
+              },
+              {
+                "quality": "500x500",
+                "url": "https://c.saavncdn.com/006/Bhoot-Part-One-The-Haunted-Ship-Hindi-2020-20200212161346-500x500.jpg"
+              }
+            ],
+            "artist": "Akhil Sachdeva",
+            "url": "https://www.jiosaavn.com/album/bhoot-part-one-the-haunted-ship/3iBAaK0xJsc_",
+            "type": "album",
+            "description": "2020 · Hindi Film · Akhil Sachdeva",
+            "year": "2020",
+            "songIds": "UleloXwf",
+            "language": "hindi"
+          },
+          {
+            "id": "62260176",
+            "title": "Channa Ve",
+            "image": [
+              {
+                "quality": "50x50",
+                "url": "https://c.saavncdn.com/729/Channa-Ve-Hindi-2025-20250223053540-50x50.jpg"
+              },
+              {
+                "quality": "150x150",
+                "url": "https://c.saavncdn.com/729/Channa-Ve-Hindi-2025-20250223053540-150x150.jpg"
+              },
+              {
+                "quality": "500x500",
+                "url": "https://c.saavncdn.com/729/Channa-Ve-Hindi-2025-20250223053540-500x500.jpg"
+              }
+            ],
+            "artist": "Ronit Vinta, Nitin Rana",
+            "url": "https://www.jiosaavn.com/album/channa-ve/5w87umyDjKE_",
+            "type": "album",
+            "description": "2025 · Hindi Album · Ronit Vinta, Nitin Rana",
+            "year": "2025",
+            "songIds": "K-vzho8L",
+            "language": "hindi"
+          }
+        ],
+        "position": 1
+      },
+      "artists": {
+        "results": [
+          {
+            "id": "12236447",
+            "title": "Verinder Channa",
+            "image": [
+              {
+                "quality": "50x50",
+                "url": "https://www.jiosaavn.com/_i/3.0/artist-default-music.png"
+              },
+              {
+                "quality": "150x150",
+                "url": "https://www.jiosaavn.com/_i/3.0/artist-default-music.png"
+              },
+              {
+                "quality": "500x500",
+                "url": "https://www.jiosaavn.com/_i/3.0/artist-default-music.png"
+              }
+            ],
+            "type": "artist",
+            "description": "Music director",
+            "position": 1
+          }
+        ],
+        "position": 4
+      },
+      "playlists": {
+        "results": [],
+        "position": 3
+      }
+    }
+  }
+
+  let jiosaavnarr = jiosaavn.data.topQuery;
+
+  jiosaavnarr.forEach((song) => {
+
+    document.write(results[0].id + "<br>")
+    
+  });
