fdfc0718cd42fc825f3ba86ade41c5adaa71c0ed
Deep Ghag <deepghag48@gmail.com>
2025-04-26 20:05:44 +0530

    Add files via upload

--- API/weather app/script.js
+++ API/weather app/script.js
@@ -2,3 +2,23 @@ const input = document.getElementById("input");
 const search = document.getElementById("search");
 
 const link = "https://api.openweathermap.org/data/2.5/weather?q=surat&appid=0695bd64a5348a29a0967425eb751c9e";
+
+async function callApi(city)
+{
+    const link = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=0695bd64a5348a29a0967425eb751c9e";   
+    console.log(link);  
+    
+    fetch(link)
+    .then((response)=>{
+        response.json()
+    })
+    .then((data)=>{
+        
+    })
+}
+
+search.addEventListener('click',()=>
+{
+    const value = input.value;
+    callApi(value);
+});
