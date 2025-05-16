e56165b69013416a78d109773a5c156bac475f83
Deep Ghag <deepghag48@gmail.com>
2025-05-16 09:18:37 +0530

    Add files via upload

--- DOM/DOM-3/index.js
+++ DOM/DOM-3/index.js
@@ -1,7 +1,58 @@
+// const form = document.getElementById("form");
+// const input = document.getElementById("task");
+// const priority = document.getElementById("priority");
+// let tbody = document.querySelector("tbody");
+
+// form.addEventListener("submit", function(event) 
+// {
+//     event.preventDefault();
+
+//     let inputTask = input.value;
+//     let prioritySelector = priority.value;
+
+//     if(inputTask === "" || prioritySelector === "")
+//     {
+//         alert("Please Enter Both Task and Priority");
+//         return;
+//     }
+
+//     let tr = document.createElement("tr");
+    
+//     let tasktd = document.createElement("td");
+//     tasktd.textContent = inputTask; 
+
+//     let prioritytd = document.createElement("td");
+//     prioritytd.textContent = prioritySelector;
+    
+//     let deletetd = document.createElement("td");
+//     deletetd.textContent = "Delete";
+//     deletetd.style.color = "red";
+//     deletetd.style.cursor = "pointer";
+
+
+//     deletetd.addEventListener('click',function()
+//     {
+//         tr.remove();
+//     })
+
+//     tr.appendChild(tasktd);
+//     tr.appendChild(prioritytd);
+//     tr.appendChild(deletetd);
+
+//     tbody.appendChild(tr);
+    
+//     input.value = "";
+//     priority.value = "";
+// })
+
+
+// onject array form
+
 const form = document.getElementById("form");
 const input = document.getElementById("task");
 const priority = document.getElementById("priority");
 let tbody = document.querySelector("tbody");
+let formData = [];
 
 form.addEventListener("submit", function(event) 
 {
@@ -10,37 +61,10 @@ form.addEventListener("submit", function(event)
     let inputTask = input.value;
     let prioritySelector = priority.value;
 
-    if(inputTask === "" || prioritySelector === "")
-    {
-        alert("Please Enter Both Task and Priority");
-        return;
-    }
+    let todo = {task : inputTask , priority : prioritySelector}
 
-    let tr = document.createElement("tr");
-    
-    let tasktd = document.createElement("td");
-    tasktd.textContent = inputTask; 
+    formData.push(todo);
 
-    let prioritytd = document.createElement("td");
-    prioritytd.textContent = prioritySelector;
-    
-    let deletetd = document.createElement("td");
-    deletetd.textContent = "Delete";
-    deletetd.style.color = "red";
-    deletetd.style.cursor = "pointer";
+    console.log(formData);
 
-
-    deletetd.addEventListener('click',function()
-    {
-        tr.remove();
-    })
-
-    tr.appendChild(tasktd);
-    tr.appendChild(prioritytd);
-    tr.appendChild(deletetd);
-
-    tbody.appendChild(tr);
-    
-    input.value = "";
-    priority.value = "";
 })
