f215387e71ecad8ba50731da91967998e23ea695
Deep Ghag <deepghag48@gmail.com>
2025-06-03 16:51:19 +0530

    Add files via upload

--- Ratio Coffee/script.js
+++ Ratio Coffee/script.js
@@ -70,7 +70,7 @@ function startAutoSlide() {
     interval = setInterval(() => {
         count = (count + 1) % images_for_slider_home_page.length;
         updateSliderContent(count);
-    }, 9000);
+    }, 11000);
 }
 
 function stopAutoSlide() {
@@ -100,3 +100,58 @@ addSliderImage();
 
 
 // makers-are-used-and-loved-slider code ends
+
+// Complete your Ratio set up slider code starts
+
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      centeredSlides: true,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+    var appendNumber = 4;
+    var prependNumber = 1;
+    document
+      .querySelector(".prepend-2-slides")
+      .addEventListener("click", function (e) {
+        e.preventDefault();
+        swiper.prependSlide([
+          '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+          '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+        ]);
+      });
+    document
+      .querySelector(".prepend-slide")
+      .addEventListener("click", function (e) {
+        e.preventDefault();
+        swiper.prependSlide(
+          '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+        );
+      });
+    document
+      .querySelector(".append-slide")
+      .addEventListener("click", function (e) {
+        e.preventDefault();
+        swiper.appendSlide(
+          '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+        );
+      });
+    document
+      .querySelector(".append-2-slides")
+      .addEventListener("click", function (e) {
+        e.preventDefault();
+        swiper.appendSlide([
+          '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+          '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+        ]);
+      });
+
+// Complete your Ratio set up slider code ends
